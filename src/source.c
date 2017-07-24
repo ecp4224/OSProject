c67d878963a870122fe9ed38f962af39ee013d00
Christopher W. Lee <pengiunn@gmail.com>
2017-07-24 16:38:08 -0400

    Create source.c

---
+++ src/source.c
@@ -0,0 +1,51 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<sys/mman.h>
+#include<sys/stat.h>
+#include<unistd.h>
+#include<fcntl.h>
+#include<pthread.h>
+
+int fd;
+char *addr;
+
+//put functions here
+
+int main(int argc, char * argv[]){
+        struct stat sb;
+        int numCPU = sysconf(_SC_NPROCESSORS_ONLN);
+        pthread_t pid1;
+        long pageSize = sysconf(_SC_PAGESIZE);
+
+        printf("Number of logical processors: %d\n", numCPU);
+
+        fd = open(argv[1], O_RDONLY);
+        if (fd==-1){
+                printf("File open failed.\n");
+                exit(EXIT_FAILURE);
+        }
+        if (fstat(fd,&sb) == 1){
+                printf("fstat error\n");
+                exit(EXIT_FAILURE);
+        }
+        chunksize = sb.st_size/4;
+
+        addr = mmap(NULL, chunksize, PROT_READ, MAP_PRIVATE, fd, 0);
+        if(addr == MAP_FAILED){
+                printf("mmap failed \n");
+                exit(EXIT_FAILURE);
+        }
+        int os = 0;
+        int i;
+        printf("Please input an integer value for the number of threads to use: ");
+        scanf("%d", &i);
+        printf("Sorting with %d threads.\n", i);
+
+        for (j = 0; j < i; i++){
+                pthread_create(&pid1, NULL, threadFunc,(void *)os);
+                os+=chunksize;
+        }
+        pthread_exit(NULL);
+        close(fd);
+        exit(EXIT_SUCCESS);
+}
